b5887f1b149db19ec6623ced9f849719b2edf7ad
Martin Wilke <miwi@ixsystems.com>
2019-06-18 22:21:20 +0800

    test

--- scripts/i3wm.sh
+++ scripts/i3wm.sh
@@ -45,4 +45,5 @@ done
 echo "Create User"
 chroot ${ISODIR} pw groupadd penbsd
 chroot ${ISODIR} pw useradd -n penbsd -m -s /usr/local/bin/zsh -G wheel,video,operator -g penbsd 
-chown -R penbsd:penbsd /usr/home/penbsd
+chroot ${ISODIR} ls -la /usr/home
+chroot ${ISODIR} chown -R penbsd:penbsd /usr/home/penbsd
